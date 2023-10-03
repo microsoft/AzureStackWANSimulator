9555cf8ab335674cc2a74a3c61a458b3d6fa42d2
Nick Liu <105009141+liunick-msft@users.noreply.github.com>
2023-10-03 10:55:41 -0700

    update sample lab config

--- config/Lab/South_WANSIM_Rack/wansim_rules.sh
+++ config/Lab/South_WANSIM_Rack/wansim_rules.sh
@@ -3,11 +3,6 @@ sudo tc qdisc add dev eth0 root handle 1: htb default 10
 sudo tc class add dev eth0 parent 1: classid 1:5 htb rate 1Gbit
 sudo tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip src 100.69.177.11/32 flowid 1:5
 
-## North
-sudo tc qdisc add dev eth0 root handle 1: htb default 10
-sudo tc class add dev eth0 parent 1: classid 1:5 htb rate 1Gbit
-sudo tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip src 100.68.177.2/32 flowid 1:5
-
 # Show Rule
 sudo tc -s qdisc show dev eth0
 sudo tc -s class show dev eth0
