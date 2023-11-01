26a891643e59925025d96a598d914775d95cf40b
Brian Houg <brhoug@microsoft.com>
2023-11-01 15:31:05 -0700

    wip

--- WanSimMgmt/WanSimMgmt.psm1
+++ WanSimMgmt/WanSimMgmt.psm1
@@ -505,12 +505,17 @@ function Get-WanSimIpAddresses {
             }
         }
         Write-Log -Message "Getting the VMNetworkAdapterInfo for '$WanSimName' on '$ownerNode'" @logParams
-        $ips = [System.Collections.ArrayList]@()
-        $vmNetworkAdapterInfo = (Get-VMNetworkAdapter -VMName $WanSimName -ComputerName $ownerNode)
-        $ips += @($vmNetworkAdapterInfo.IPAddresses | Where-Object { $_ -notmatch '^fe80:' })
-        $ips | Where-Object { Write-Log -Message "IP Address is $_" @logParams }
         
-        return $ips
+        $vmNetworkAdapterInfo = (Get-VMNetworkAdapter -VMName $WanSimName -ComputerName $ownerNode)
+        $ipAddresses = [System.Collections.ArrayList]@()
+        $filteredIPs = $vmNetworkAdapterInfo.IPAddresses | Where-Object { $_ -notmatch '^fe80:' }
+        foreach ($ip in $filteredIPs) {
+            Write-Log -Message "IP address for '$WanSimName' is '$ip'" @logParams
+            Write-Log -Message "Adding IP address '$ip' to the list of IP addresses" @logParams
+            $ipAddresses.Add($ip)
+        }
+        Write-Log -Message "type is '$($ipAddresses.Gettype())'" @logParams
+        return $ipAddresses
     }
     catch {
 
@@ -524,3 +529,21 @@ function Get-WanSimIpAddresses {
     }
 
 }
+
+function BuildIfChanged {
+    $thisVersion = Get-Version
+    $nextVersion = Get-NextVersion
+    if ($thisVersion -ne $nextVersion) { Build $nextVersion }
+    return $nextVersion
+}
+
+function Build ($version) {
+    Write-Host "a build was run for version: $version"
+}
+
+# Actual definitions of Get-Version and Get-NextVersion are not shown here,
+# since we'll just be mocking them anyway. However, the commands do need to
+# exist in order to be mocked, so we'll stick dummy functions here
+
+function Get-Version { return 0 }
+function Get-NextVersion { return 0 }
